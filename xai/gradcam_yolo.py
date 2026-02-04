190f0d1e24ff94c21e591873891eac8dc471abe4
Seongjin Ye <dolcei5935@gmail.com>
2026-02-04 17:09:13 +0900

    Squashed 'drift_detection/new-license-plate-detection/' changes from 0789441..b7f24c3
    b7f24c3 Auto update from sync_subtree
    
    git-subtree-dir: drift_detection/new-license-plate-detection
    git-subtree-split: b7f24c3dd72320780936d78cd5c4402dc25daa11

--- xai/gradcam_yolo.py
+++ xai/gradcam_yolo.py
@@ -1,41 +1,78 @@
 import torch
 import torch.nn.functional as F
 
+
 class YOLOGradCAM:
-    def __init__(self, model, target_layers):
+    """
+    Grad-CAM for DiL (YOLO26).
+    - Hook: backbone / neck conv feature
+    - Target: sum of objectness outputs (Mo)
+    """
+
+    def __init__(self, model, target_layer):
         self.model = model
-        self.target_layers = target_layers
-        self.activations = []
-        self.gradients = []
+        self.target_layer = target_layer
+        self.activation = None
+        self.gradient = None
 
-        for l in self.target_layers:
-            l.register_forward_hook(self._forward_hook)
-            l.register_backward_hook(self._backward_hook)
+        self._register_hooks()
 
-    def _forward_hook(self, m, i, o):
-        self.activations.append(o)
+    def _register_hooks(self):
+        def forward_hook(module, inp, out):
+            self.activation = out
 
-    def _backward_hook(self, m, gi, go):
-        self.gradients.append(go[0])
+        def backward_hook(module, grad_in, grad_out):
+            self.gradient = grad_out[0]
 
-    def saliency(self, det_raw):
+        self.target_layer.register_forward_hook(forward_hook)
+        self.target_layer.register_backward_hook(backward_hook)
+
+    def _aggregate_objectness(self, det_raw):
         """
-        Eq.(1): sum(objectness)
+        Implements Eq.(1) in DiL paper:
+        sum of all objectness scores
         """
         score = 0.0
-        for out in det_raw:
-            score += out.sum()
 
+        if isinstance(det_raw, (list, tuple)):
+            for out in det_raw:
+                # out shape depends on head
+                # YOLO26 one-to-one: (B, 300, 6)
+                # assume objectness at index 4
+                score += out[..., 4].sum()
+        else:
+            score = det_raw[..., 4].sum()
+
+        return score
+
+    def saliency(self, det_raw, input_shape):
+        """
+        Returns:
+            cam: (1, 1, H, W)
+        """
+        # 1. aggregate objectness
+        score = self._aggregate_objectness(det_raw)
+
+        # 2. backward
         self.model.zero_grad()
         score.backward(retain_graph=True)
 
-        cams = []
-        for act, grad in zip(self.activations, self.gradients):
-            w = grad.mean(dim=(2, 3), keepdim=True)
-            cam = (w * act).sum(dim=1, keepdim=True)
-            cams.append(F.relu(cam))
+        # 3. Grad-CAM
+        grad = self.gradient          # (B, C, h, w)
+        act = self.activation         # (B, C, h, w)
 
-        cam = sum(F.interpolate(c, size=cams[0].shape[-2:], mode="bilinear", align_corners=False)
-                  for c in cams)
+        weights = grad.mean(dim=(2, 3), keepdim=True)
+        cam = (weights * act).sum(dim=1, keepdim=True)
+        cam = F.relu(cam)
+
+        # 4. upsample to input size
+        cam = F.interpolate(
+            cam,
+            size=input_shape[-2:],
+            mode="bilinear",
+            align_corners=False,
+        )
+
+        # 5. normalize
         cam = (cam - cam.min()) / (cam.max() - cam.min() + 1e-6)
         return cam
