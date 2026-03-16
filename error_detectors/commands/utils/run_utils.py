9c42f53a7192a2037ad1eafd2c1d596bfe001a6a
Seongjin Ye <dolcei5935@gmail.com>
2026-03-16 20:14:17 +0900

    Squashed 'drift_detection/new-license-plate-detection/' changes from bf4b49d9..d5d0e224
    d5d0e224 Auto update from sync_subtree
    
    git-subtree-dir: drift_detection/new-license-plate-detection
    git-subtree-split: d5d0e22491d7e5e9a0af12d46c63b687ee7ff07f

--- error_detectors/commands/utils/run_utils.py
+++ error_detectors/commands/utils/run_utils.py
@@ -9,7 +9,7 @@ def create_run_dir(model_group: str, cue: str) -> Path:
     run_name = datetime.now().strftime("%m-%d-%Y_%H;%M")
     group = str(model_group).strip().lower()
     cue_name = str(cue).strip().lower()
-    run_dir = PROJECT_ROOT / "runs" / group / f"{run_name}_{cue_name}"
+    run_dir = PROJECT_ROOT / "runs" / group / cue_name / run_name
     run_dir.mkdir(parents=True, exist_ok=True)
     return run_dir
 
