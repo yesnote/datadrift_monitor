b4672bab5048aa4bdc18f36abdda252c5ad06d5d
Seongjin Ye <dolcei5935@gmail.com>
2026-03-16 18:16:45 +0900

    Squashed 'drift_detection/new-license-plate-detection/' changes from e9cf0d4..bc7e8cc
    bc7e8cc Auto update from sync_subtree
    
    git-subtree-dir: drift_detection/new-license-plate-detection
    git-subtree-split: bc7e8cc47ca7aca4ee7c0678f107fcba502d48e6

--- error_detectors/losses/loss.py
+++ error_detectors/losses/loss.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import numpy as np
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import average_precision_score, roc_auc_score
 
 
 def evaluate_classifier(y_true: np.ndarray, y_score: np.ndarray) -> tuple[float, float]:
-    return accuracy_score(y_true, y_score >= 0.5), roc_auc_score(y_true, y_score)
+    return roc_auc_score(y_true, y_score), average_precision_score(y_true, y_score)
